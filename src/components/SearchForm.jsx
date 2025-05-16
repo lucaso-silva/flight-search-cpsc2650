28f4142c055d97380cf2ce44ad2773767f05899f
Lucas Oliveira <luc.osilva@gmail.com>
2025-05-16 00:47:06 -0700

    Add spinner

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,29 +1,42 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Spinner from 'react-bootstrap/Spinner';
+import Button from 'react-bootstrap/Button';
 
 export default function SearchForm(){
     const [ from, setFrom ] = useState('');
     const [ to, setTo ] = useState('');
+    const [ isLoading, setIsLoading ] = useState(false);
     const navigate = useNavigate();
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        navigate(`/results?from=${from}&to=${to}`);
+        setIsLoading(true);
+        setTimeout(()=>{
+            navigate(`/results?from=${from}&to=${to}`);
+        }, 1000)
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                From:
-                <input type="text" value={from} onChange={(e) => setFrom(e.target.value)} required={true}/>
-            </label>
-            <br/>
-            <label>
-                To:
-                <input type="text" value={to} onChange={(e) => setTo(e.target.value)} required={true}/>
-            </label>
-            <br/>
-            <button type="submit" >Search Flights</button>
-        </form>
+        <>
+            { isLoading ?
+                <Spinner animation="border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </Spinner> :
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        From:
+                        <input type="text" value={from} onChange={(e) => setFrom(e.target.value)} required={true}/>
+                    </label>
+                    <br/>
+                    <label>
+                        To:
+                        <input type="text" value={to} onChange={(e) => setTo(e.target.value)} required={true}/>
+                    </label>
+                    <br/>
+                    <Button variant="primary" type="submit">Search Flights</Button>
+                </form>}
+        </>
+
     );
 }
